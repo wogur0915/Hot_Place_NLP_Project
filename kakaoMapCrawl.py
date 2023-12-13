e0568f2c929bc02e19de486438e8355359d34ff2
wogur0915 <wogur0915@gmail.com>
2023-12-13 17:10:22 +0900

    크롬 드라이버 경로 설정

--- kakaoMapCrawl.py
+++ kakaoMapCrawl.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#import seaborn as sns
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -10,5 +13,10 @@ from bs4 import BeautifulSoup
 import re
 import time
 
+excutable_path = 'chromedriver2.exe'
+
 # 크롤링할 사이트 주소 = 카카오맵
 url = "https://map.kakao.com/"
+
+driver = webdriver.Chrome(executable_path=excutable_path)
+driver.get(url)
