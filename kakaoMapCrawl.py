0bcdd32ad6a3513eff61d9eb0cb625f4f7791837
wogur0915 <wogur0915@gmail.com>
2023-12-13 17:18:25 +0900

    크롬 드라이버 매니져 자동설치 라이브러리 추가

--- kakaoMapCrawl.py
+++ kakaoMapCrawl.py
@@ -1,7 +1,4 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-#import seaborn as sns
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -9,14 +6,25 @@ warnings.filterwarnings("ignore")
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
 from bs4 import BeautifulSoup
 import re
 import time
 
-excutable_path = 'chromedriver2.exe'
-
 # 크롤링할 사이트 주소 = 카카오맵
 url = "https://map.kakao.com/"
 
-driver = webdriver.Chrome(executable_path=excutable_path)
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
 driver.get(url)
+
+# 검색어를 입력
+searchbox = driver.find_element(By.XPATH, "//input[@id='search.keyword.query']")
+searchbox.send_keys("충북대 맛집")
+
+# 검색
+searchbutton = driver.find_element(By.XPATH, "//button[@id='search.keyword.submit']")
+driver.execute_script("arguments[0].click();", searchbutton)
+time.sleep(2)
